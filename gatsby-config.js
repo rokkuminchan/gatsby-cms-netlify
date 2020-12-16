b1774a03f22ea7d13d83ffdf173936d15783f35e
rokkuminchan <loc@world-works.co.jp>
2020-12-16 17:51:36 +0900

    Add UUID control to CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`
+    {
+      resolve: `gatsby-plugin-netlify-cms`,
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
